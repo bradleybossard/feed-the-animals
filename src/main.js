abe3591f478eb47f911eb4d0223a16c3002d0901
Bradley Bossard <bradleybossard@gmail.com>
2025-07-26 14:58:04 -0700

    add game logic

--- src/main.js
+++ src/main.js
@@ -1,24 +1,227 @@
+
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import * as THREE from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';   
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils';
 
-setupCounter(document.querySelector('#counter'))
+let elThreejs = document.getElementById("threejs");
+let camera,scene,renderer;
+let axesHelper;
+
+let keyboard = {};
+let playerMesh;
+
+let projectileMeshes = [];
+let projectileMesh;
+
+let animalMeshes = [];
+let animalMesh;
+
+let animalGLTF;
+let mixers = [];
+let clock;
+init();
+
+async function init() {
+
+  // Scene
+	scene = new THREE.Scene();
+
+  // Camera
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.z = -10;
+	// camera.position.z = 0;
+	camera.position.y = 20;
+  
+	// rotate camera to see the scene from the top
+	camera.rotation.x = -Math.PI / 2;
+
+	const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820);
+	ambient.position.set(0, 5, 0);
+	scene.add(ambient);
+  
+	clock = new THREE.Clock();
+
+  // render
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMap.enabled = true;
+	renderer.outputEncoding = THREE.sRGBEncoding;
+
+
+	// axesHelper = new THREE.AxesHelper( 100 );
+	// scene.add( axesHelper );
+
+	elThreejs.appendChild(renderer.domElement);
+
+	await loadAnimal();
+
+	addPlayer();
+	addPlane();
+	addProjectile();
+
+
+	addKeysListener();
+	animate();
+
+	spawnAnimals();
+
+}
+
+function animate(){
+
+	movePlayer();
+	updateProjectiles();
+	updateAnimals();
+
+
+	// collision detection between projectileMeshes and animalMeshes
+	checkCollisions();
+
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+
+	const dt = clock.getDelta();
+	for ( const mixer of mixers ) mixer.update( dt );
+}
+
+async function addPlayer(){
+	const gltfLoader = new GLTFLoader().setPath( '' );
+	const playerGLTF = await gltfLoader.loadAsync( 'player.glb' );
+	playerMesh = playerGLTF.scene.children[0];
+	scene.add(playerMesh);
+}
+
+function addPlane(){
+	const texture = new THREE.TextureLoader().load( "src/assets/Texture_Grass.png" );
+	let geometry =  new THREE.BoxGeometry(50, 0, 50);
+	let material = new THREE.MeshBasicMaterial({map: texture});
+	let plane = new THREE.Mesh(geometry, material);
+	plane.position.set(0, 0, -10);
+	scene.add(plane);
+  }
+
+
+function addKeysListener(){
+	window.addEventListener('keydown', function(event){
+	  keyboard[event.keyCode] = true;
+	} , false);
+	window.addEventListener('keyup', function(event){
+	  keyboard[event.keyCode] = false;
+	} , false);
+
+	window.addEventListener("keyup", (event) => {
+		// boiler plate code to prevent side effects
+		if (event.isComposing || event.keyCode === 229) {
+		  return;
+		}
+	
+		// space bar 
+		if (event.keyCode == 32) {
+		  let projectileMeshClone = projectileMesh.clone();
+		  projectileMeshClone.position.x = playerMesh.position.x;
+		  projectileMeshClone.position.y = playerMesh.position.y;
+		  projectileMeshClone.position.z = playerMesh.position.z;
+		  scene.add(projectileMeshClone);
+		  projectileMeshes.push(projectileMeshClone);
+		}
+	  });
+}
+
+
+
+function movePlayer(){
+	// left letter A
+	if(keyboard[65] && playerMesh.position.x > -15) playerMesh.position.x -= 0.25;
+	// right letter D
+	if(keyboard[68] && playerMesh.position.x < 15) playerMesh.position.x += 0.25;
+}
+
+
+async function addProjectile(){
+	//const gltfLoader = new GLTFLoader().setPath( 'src/assets/' );
+	const gltfLoader = new GLTFLoader().setPath( '' );
+	const projectileGLTF = await gltfLoader.loadAsync( 'pizza.glb' );
+	projectileMesh = projectileGLTF.scene;
+	projectileMesh.scale.set(2, 2, 2);
+}
+
+function updateProjectiles(){
+	projectileMeshes.forEach((projectile, index) => {
+		projectile.position.z -= 0.5;
+		if(projectile.position.z < -20){
+			scene.remove(projectile);
+			projectileMeshes.splice(index, 1);
+		  }
+	});
+}
+
+async function loadAnimal(){
+	const gltfLoader = new GLTFLoader().setPath('');
+	animalGLTF = await gltfLoader.loadAsync( 'moose.glb' );
+}
+
+
+function addAnimal(posX){
+	let model1 = SkeletonUtils.clone(animalGLTF.scene);
+
+	let animations = {};
+	animalGLTF.animations.forEach( animation => {
+	  animations[animation.name] = animation;
+	});
+
+	let actualAnimation = "MooseAnimation";
+	const mixer1 = new THREE.AnimationMixer(model1);
+	mixer1.clipAction(animations[actualAnimation]).play();
+  
+	model1.position.x = posX;
+	model1.position.y = 0;
+	model1.position.z = -30;
+
+	animalMeshes.push(model1);
+	scene.add(model1);
+	mixers.push(mixer1);
+}
+
+function spawnAnimals(){
+	// random number between -20 and 20
+	let randomX = Math.floor(Math.random() * 20) - 10;
+	addAnimal(randomX);
+	setInterval(() => {
+		randomX = Math.floor(Math.random() * 20) - 10;
+		addAnimal(randomX);
+	}, 2000);
+  }
+  
+function updateAnimals(){
+	animalMeshes.forEach((animal, index) => {
+		animal.position.z += 0.15;
+		if(animal.position.z > 0){
+		  scene.remove(animal);
+		  animalMeshes.splice(index, 1);
+		}
+	});
+}
+  
+function checkCollisions(){
+	animalMeshes.forEach((animal, indexa) => {
+		projectileMeshes.forEach((projectile, indexb) => {
+			if( animal.position.x >= projectile.position.x - 1 &&
+				animal.position.x <= projectile.position.x + 1 &&
+				animal.position.z >= projectile.position.z - 1 &&
+				animal.position.z <= projectile.position.z + 1){
+					scene.remove(animal);
+					animalMeshes.splice(indexa, 1);
+					scene.remove(projectile);
+					projectileMeshes.splice(indexb, 1);
+			}
+		});
+	});
+}
